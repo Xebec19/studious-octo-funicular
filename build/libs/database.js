c9af11fe80b0d787965c98d4afa29f42b76c6dc9
xebec19 <rohant946@gmail.com>
2022-02-21 23:04:08 +0530

    set database connection string

--- build/libs/database.js
+++ build/libs/database.js
@@ -1,21 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbQuery = void 0;
-var environment_1 = require("../environment");
+// import { dbDatabase, dbHost, dbPassword, dbPort, dbUser } from "../environment";
 var Pool = require("pg").Pool;
-var config = {
-    user: "".concat(environment_1.dbUser),
-    host: "".concat(environment_1.dbHost),
-    database: "".concat(environment_1.dbDatabase),
-    password: "".concat(environment_1.dbPassword),
-    port: "".concat(environment_1.dbPort),
-};
 // const config = {
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
+//   user: `${dbUser}`,
+//   host: `${dbHost}`,
+//   database: `${dbDatabase}`,
+//   password: `${dbPassword}`,
+//   port: `${dbPort}`,
 // };
+var config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+};
 var pool = new Pool(config);
 pool.on("error", function (err, client) {
     console.error("Error:", err);
